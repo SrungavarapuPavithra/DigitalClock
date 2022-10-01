8c5059d43fe04a87da197ee9b685279921eb62c7
SrungavarapuPavithra <96069408+SrungavarapuPavithra@users.noreply.github.com>
2022-10-01 09:12:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import DigitalClock from "./components/DigitalClock.js";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <DigitalClock />
+    </div>
+  );
+}
+export default App;
